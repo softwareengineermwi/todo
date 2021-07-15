412d8abebb05e5e9195d0a0c7474b0e4f9fbea30
Mwila Kaunda <mbkcust@gmail.com>
2021-07-15 17:23:38 +0200

    Fix js linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-globals */
+/* eslint-disable no-plusplus */
 import './style.css'
 
 function g(e) {
@@ -20,16 +21,14 @@ const tasks = [
     description: "Sleep more, again",
     completed: false,
     index: 0
-  }
-]
+  },
+];
 
 function loadTodo() {
   for (let x = 0; x < tasks.length; x++) {
-    let task = tasks[x]
-
-    let template = document.createElement('template')
-
-    let templateString = `<li class="flex items-center">
+    const task = tasks[x];  
+    const template = document.createElement('template');
+    const templateString = `<li class="flex items-center">
       <div class="flex items-center width-100%">
         <div class="flex items-center space-between">
           <div class="clearance-right"><input type="checkbox"></div>
@@ -38,13 +37,14 @@ function loadTodo() {
           </div>
         </div>
         <div class="flex grow justify-end"><button class="bg-none material-icons border-none">more_vert</button></div>
-    </li>`
+    </li>`;
 
-    template.innerHTML = templateString
+    template.innerHTML = templateString;
 
-    g('todo_list').appendChild(template.content.firstChild)
+    g('todo_list').appendChild(template.content.firstChild);
   }
 }
 
 onload = loadTodo();
+/* eslint-disable no-plusplus */
 /* eslint-disable no-restricted-globals */
