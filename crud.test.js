9834027f7ea3ea2376c0d8977d161ac759c76455
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-22 15:01:36 +0200

    add func to the test

--- crud.test.js
+++ crud.test.js
@@ -1,33 +1,33 @@
-const { del } = require('./src/crud')
+const { del } = require('./src/crud');
 
 test('Should delete item from localstorage', () => {
   let tasks = [
     {
-      description: "some description",
+      description: 'some description',
       completed: false,
-      index: 1
+      index: 1,
     },
     {
-      description: "some more description",
+      description: 'some more description',
       completed: false,
-      index: 2
+      index: 2,
     },
     {
-      description: "some even more description",
+      description: 'some even more description',
       completed: false,
-      index: 3
+      index: 3,
     },
     {
-      description: "some even, yes, more description",
+      description: 'some even, yes, more description',
       completed: false,
-      index: 4
-    }
-  ]
+      index: 4,
+    },
+  ];
 
-  let btn = document.createElement('button')
-  btn.value = 0
+  let btn = document.createElement('button');
+  btn.value = 0;
 
-  tasks = del(btn, true, tasks)
+  // tasks = del(btn, true, tasks);
 
-  expect(tasks.length).toBe(tasks.length - 1)
-})
+  expect(del(tasks, 0).length).toBe(tasks.length - 1);
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { dragstart, dragover, drop, dragleave } from './draggability';
 import { onchange } from './status';
-import { apply, clear, updateIndices, del, add } from './crud'
+import { apply, clear, updateIndices, del, add } from './crud';
 
 function g(e) {
   return document.getElementById(e);
@@ -11,7 +11,7 @@ function loadTodo() {
   if (localStorage.getItem('things') !== null) {
     const tasks = JSON.parse(localStorage.getItem('things'));
 
-    console.log(tasks)
+    console.log(tasks);
 
     for (let x = 0; x < tasks.length; x++) {
       const task = tasks[x];
@@ -23,75 +23,83 @@ function loadTodo() {
       tasktemplate.draggable = true;
       tasktemplate.addEventListener('dragstart', dragstart);
 
-      let dropArea = document.createElement('div')
+      let dropArea = document.createElement('div');
 
-      let container1 = document.createElement('div')
-      let container2 = document.createElement('div')
-      let container3 = document.createElement('div')
+      let container1 = document.createElement('div');
+      let container2 = document.createElement('div');
+      let container3 = document.createElement('div');
 
-      let btn = document.createElement('button')
-      let input = document.createElement('input')
+      let btn = document.createElement('button');
+      let input = document.createElement('input');
 
-      let h4 = document.createElement('h4')
-      h4.innerText = task.description
-      h4.contentEditable = true
+      let h4 = document.createElement('h4');
+      h4.innerText = task.description;
+      h4.contentEditable = true;
 
-      h4.setAttribute('data-id', x)
-      h4.className = "x width-100%"
+      h4.setAttribute('data-id', x);
+      h4.className = 'x width-100%';
 
       if (task.completed) {
-        h4.classList.add('strike')
+        h4.classList.add('strike');
       }
 
-      container1.className = "flex items-center width-100%"
-      container2.className = "flex items-center space-between"
-      container3.className = "flex grow justify-end"
+      container1.className = 'flex items-center width-100%';
+      container2.className = 'flex items-center space-between';
+      container3.className = 'flex grow justify-end';
 
-      input.className = "x clearance-right"
+      input.className = 'x clearance-right';
 
-      dropArea.className = "drop-area"
-      btn.value = x
-      btn.className = "x bg-none material-icons border-none"
-      btn.innerText = 'more_vert'
+      dropArea.className = 'drop-area';
+      btn.value = x;
+      btn.className = 'x bg-none material-icons border-none';
+      btn.innerText = 'more_vert';
 
-      h4.addEventListener('input', e => {
-        apply(e.target.textContent, e.target.getAttribute('data-id'))
-      })
+      h4.addEventListener('input', (e) => {
+        apply(e.target.textContent, e.target.getAttribute('data-id'));
+      });
 
       h4.addEventListener('click', () => {
-        del(btn, true)
-      })
+        // localStorage.setItem('things', JSON.stringify(del(btn, true, tasks)));
+        // location.reload();
+        btn.innerText = 'delete';
+        btn.addEventListener('click', () => {
+          localStorage.setItem('things', JSON.stringify(del(tasks, btn.value)));
+          location.reload();
+        });
+      });
 
       h4.addEventListener('focusout', () => {
-        del(btn, false)
-      })
+        del(btn, false, tasks);
+      });
 
-      container2.append(input, h4)
-      container3.appendChild(btn)
+      container2.append(input, h4);
+      container3.appendChild(btn);
 
-      container1.append(container2, container3)
-      tasktemplate.append(dropArea, container1)
+      container1.append(container2, container3);
+      tasktemplate.append(dropArea, container1);
 
-      dropArea.setAttribute('data-id', x)
-      input.type = 'checkbox'
-      input.setAttribute('data-id', x)
-      input.checked = task.completed
-      input.addEventListener('change', onchange)
+      dropArea.setAttribute('data-id', x);
+      input.type = 'checkbox';
+      input.setAttribute('data-id', x);
+      input.checked = task.completed;
+      input.addEventListener('change', onchange);
 
-      dropArea.addEventListener('dragover', dragover)
-      dropArea.addEventListener('dragleave', dragleave)
+      dropArea.addEventListener('dragover', dragover);
+      dropArea.addEventListener('dragleave', dragleave);
       dropArea.addEventListener('drop', drop);
 
       g('todo_list').appendChild(tasktemplate);
     }
 
-    g('clear').addEventListener('click', clear)
+    g('clear').addEventListener('click', clear);
   }
 }
 
 g('form').addEventListener('submit', (e) => {
-  e.preventDefault()
-  add(g('input-description').value)
-})
+  e.preventDefault();
+  add(g('input-description').value);
+});
 
-onload = updateIndices(() => { loadTodo() });
+onload = updateIndices(() => {
+  loadTodo();
+});
